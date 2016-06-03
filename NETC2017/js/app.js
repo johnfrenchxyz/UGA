96d1bf09879795cb9637a3a6865372ffc101ad62
John French <hello@johnfrench.xyz>
2016-06-03 14:10:20 -0400

    Updated the NECT site with a scrolling effect

--- NETC2017/js/app.js
+++ NETC2017/js/app.js
@@ -1,1 +1,10 @@
 $(document).foundation();
+
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+$("#findOutMore").click(function() {
+   scrollToAnchor('start');
+});
