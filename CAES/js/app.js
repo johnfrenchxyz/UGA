552218469a264146679b0fbed0a8100ed596b37b
John French <hello@johnfrench.xyz>
2016-09-23 08:34:21 -0400

    Makes CAES search more functional

--- CAES/js/app.js
+++ CAES/js/app.js
@@ -2,24 +2,38 @@ $(document).foundation();
 
 // Search
 //-------
+// $('#searchTrigger').on('click', function() {
+//     // Hide the "People" Menu
+//     $('.homepage-people-menu').hide().addClass('is-hidden');
+//     // Hide the Search Trigger
+//     $('#searchTrigger').hide();
+//     // Show the Search Form
+//     $('.search-form').show();
+//     // Focus the cursor in the search field
+//     $('#searchField').focus();
+//     // Close the Search
+//     $('#searchClose').on('click', function() {
+//         // Hide the search form
+//         $('.search-form').hide();
+//         // Show the "People" menu
+//         $('.homepage-people-menu').show().removeClass('is-hidden');
+//         // Show the Search Trigger
+//         $('#searchTrigger').show();
+//     });
+// });
+
 $('#searchTrigger').on('click', function() {
-    // Hide the "People" Menu
-    $('.homepage-people-menu').hide().addClass('is-hidden');
-    // Hide the Search Trigger
-    $('#searchTrigger').hide();
-    // Show the Search Form
-    $('.search-form').show();
-    // Focus the cursor in the search field
+    var $icon = $('#searchTrigger i');
+    $icon.toggleClass('is-active')
+    if ( $icon.hasClass('is-active') ) {
+        $icon.text('close');
+    } else {
+        $icon.text('search');
+    }
+    // Show Form
+    $('.search-form').slideToggle('fast');
+    // Focus on Form
     $('#searchField').focus();
-    // Close the Search
-    $('#searchClose').on('click', function() {
-        // Hide the search form
-        $('.search-form').hide();
-        // Show the "People" menu
-        $('.homepage-people-menu').show().removeClass('is-hidden');
-        // Show the Search Trigger
-        $('#searchTrigger').show();
-    });
 });
 
 // Drilldown Height Fix -- Not sure why this fix is needed, but it is.
