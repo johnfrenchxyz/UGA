3a8b5231e58a5a80a5017f7406eae8c6db60c669
John French <hello@johnfrench.xyz>
2016-09-26 12:43:20 -0400

    Adds extesnion dropdown for testing

--- Extension/county-template/js/app.js
+++ Extension/county-template/js/app.js
@@ -2,5 +2,13 @@ $(document).foundation();
 
 // Search
 $('.search-trigger a').on('click', function() {
-    
+    var $icon = $('.search-trigger i');
+    if ( $icon.text() === 'search' ) {
+        $icon.text('close');
+    } else {
+        $icon.text('search');
+    }
+    $('.sub-menu').slideToggle(200);
+    // Focus on Form
+    $('#searchField').focus();
 });
